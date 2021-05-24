c5d0cff60b91beebea7d89873526bb79df64a66a
ingrid1218 <60760986+ingrid1218@users.noreply.github.com>
2021-05-24 14:36:55 -0300

    Add files via upload

---
+++ Kalc/js/kalcWeb.js
@@ -0,0 +1,92 @@
+console.log("Teste Calculadora");
+
+//DOM
+
+// OPERAÇÕES
+const igual = document.getElementById("igual");
+const soma = document.getElementById("soma");
+const subt = document.getElementById("subt");
+const mult = document.getElementById("mult");
+const divi = document.getElementById("divi");
+
+//NUMEROS
+const um = document.getElementById("um");
+const dois = document.getElementById("dois");
+const tres = document.getElementById("tres");
+const quatro = document.getElementById("quatro");
+const cinco = document.getElementById("cinco");
+const seis = document.getElementById("seis");
+const sete = document.getElementById("sete");
+const oito = document.getElementById("oito");
+const nove = document.getElementById("nove");
+const limpar = document.getElementById("limpar");
+const ponto = document.getElementById("ponto");
+
+//EVENTOS
+igual.onclick = function(){
+    console.log(igual.innerText);
+};
+
+soma.onclick = function(){
+    console.log(soma.innerText);
+};
+
+subt.onclick = function(){
+    console.log(subt.innerText);
+};
+
+mult.onclick = function(){
+    console.log(mult.innerText);
+};
+
+divi.onclick = function(){
+    console.log(divi.innerText);
+};
+
+zero.onclick = function(){
+    console.log(zero.innerText);
+};
+
+um.onclick = function(){
+    console.log(um.innerText);
+};
+
+dois.onclick = function(){
+    console.log(dois.innerText);
+};
+
+tres.onclick = function(){
+    console.log(tres.innerText);
+};
+
+quatro.onclick = function(){
+    console.log(quatro.innerText);
+};
+
+cinco.onclick = function(){
+    console.log(cinco.innerText);
+};
+
+seis.onclick = function(){
+    console.log(seis.innerText);
+};
+
+sete.onclick = function(){
+    console.log(sete.innerText);
+};
+
+oito.onclick = function(){
+    console.log(oito.innerText);
+};
+
+nove.onclick = function(){
+    console.log(nove.innerText);
+};
+
+limpar.onclick = function(){
+    console.log(limpar.innerText);
+};
+
+ponto.onclick = function(){
+    console.log(ponto.innerText);
+};
